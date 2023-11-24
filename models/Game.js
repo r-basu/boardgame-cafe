1d81ec944fed354efa91c13831f7810972080b32
afzama <141961264+afzama@users.noreply.github.com>
2023-11-23 19:48:24 -0500

    game model

---
+++ models/Game.js
@@ -0,0 +1,44 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Game extends Model { }
+
+Game.init({
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    isAvailable: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: true
+    },
+    minPlayers: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    maxPlayers: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    minAge: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    minTime: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    maxTime: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    description: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, {
+    sequelize
+});
+
+module.exports = Game

ff4a597f33311941d2a1fa0a141a89390f86037f
Rahul Basu <142247733+r-basu@users.noreply.github.com>
2023-11-23 19:12:29 -0500

    add initial game/user routes

---
+++ controllers/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const {User,Game, Review} = require("../models")
+const gameRoutes = require("./gameRoutes");
+const userRoutes = require("./userRoutes")
+
+router.get("/",(req,res)=>{
+  res.render("home")
+})
+
+router.get("/login",(req,res)=>{
+  if(req.session.user){
+      res.redirect("/profile")
+  }
+  res.render("login")
+})

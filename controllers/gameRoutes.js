52884ff81387818bcb573bbf5250a88b4ef11789
afzama <141961264+afzama@users.noreply.github.com>
2023-11-29 21:47:44 -0500

    gamrRoutes update to setup handlebars engine

--- controllers/gameRoutes.js
+++ controllers/gameRoutes.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const exphbs = require("express-handlebars");
+const app = express();
 const { Game, User, Category } = require("../models");
+
+// Set the handlebars engine
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
 
 //Find All Games
 router.get("/", (req, res) => {
@@ -8,7 +14,7 @@ router.get("/", (req, res) => {
     include: [Category]
   })
     .then((dbGames) => {
-      res.json(dbGames);
+      res.render("games", { games: dbGames });
     })
     .catch((err) => {
       res.status(500).json({ msg: "oh no!", err });

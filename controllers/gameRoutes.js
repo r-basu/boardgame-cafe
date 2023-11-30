684508a6f0a801c21406bfaedd5f875629d0de9c
DavidRodriguez119 <santiago1.dsrr@gmail.com>
2023-11-30 00:19:23 -0500

    fixed gameRoutes file

--- controllers/gameRoutes.js
+++ controllers/gameRoutes.js
@@ -1,12 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const exphbs = require("express-handlebars");
-const app = express();
 const { Game, User, Category } = require("../models");
-
-// Set the handlebars engine
-app.engine("handlebars", exphbs());
-app.set("view engine", "handlebars");
 
 //Find All Games
 router.get("/", (req, res) => {
@@ -14,7 +8,7 @@ router.get("/", (req, res) => {
     include: [Category]
   })
     .then((dbGames) => {
-      res.render("games", { games: dbGames });
+      res.json(dbGames);
     })
     .catch((err) => {
       res.status(500).json({ msg: "oh no!", err });

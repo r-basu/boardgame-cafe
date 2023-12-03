44ba3e088fa79abf0bca0ee7ffb66bf25900cf88
DavidRodriguez119 <santiago1.dsrr@gmail.com>
2023-12-03 17:26:38 -0500

    fetch user information for profile page

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,21 +1,23 @@
 // Fetch User data
 // fetch boardgames data
+let userInfo;
 
-fetch(`/api/users/session/{userData.id}`)
+fetch(`/api/users/session/1`)
   .then(response => response.json())
   .then(userData => {
     console.log(`User data retrieved`);
-    console.log(userData)
+    userInfo = userData
+    console.log(userInfo)
   })
   .catch(error => {
     console.error('Error trying to fetch user data', error);
   });
 
-//TODO FIX NOT WORKING
-document.querySelector("#unclaim-btn").addEventListener("submit", e => {
-  fetch(`/api/users/deleteCurrentGame/`)
-    .then(response => response.json())
-    .catch(error => {
-      console.error('Error trying to fetch user data', error);
-    });
-})
+// //TODO FIX NOT WORKING
+// document.querySelector("#unclaim-btn").addEventListener("submit", e => {
+//   fetch(`/api/users/deleteCurrentGame/`)
+//     .then(response => response.json())
+//     .catch(error => {
+//       console.error('Error trying to fetch user data', error);
+//     });
+// })
